9fbc50d39a5756d69ba79f4aa58ef98ad5453aa9
Prathamesh131290 <prathamesh.ppg90@gmail.com>
2026-02-07 15:09:02 +0530

    Updated Test Runner

--- APIMockBeeceptor/src/test/java/cucumber/Options/TestRunner.java
+++ APIMockBeeceptor/src/test/java/cucumber/Options/TestRunner.java
@@ -7,5 +7,5 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/java/features",glue= {"stepDefinitions"})
 public class TestRunner {
-
+	//Using JUnit
 }
