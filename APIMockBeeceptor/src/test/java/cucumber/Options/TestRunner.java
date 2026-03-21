862e7f0683d4d651d5ee2a6d8250348040ef31e0
Prathamesh131290 <prathamesh.ppg90@gmail.com>
2026-03-21 13:13:57 +0530

    updated test runner by including reporting

--- APIMockBeeceptor/src/test/java/cucumber/Options/TestRunner.java
+++ APIMockBeeceptor/src/test/java/cucumber/Options/TestRunner.java
@@ -7,7 +7,8 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features="src/test/java/features",
-		glue= {"stepDefinitions"}
+		glue={"stepDefinitions"},
+		plugin= {"pretty","html:target/cucumber-html-report.html"}
 )
 public class TestRunner {
 	//Using JUnit
